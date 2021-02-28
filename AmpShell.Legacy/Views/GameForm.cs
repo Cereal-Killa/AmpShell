d7c7e3bf14a2731befb2f143fc767c32b9fb7eed
Maximilien Noal <noal.maximilien@gmail.com>
2021-02-28 09:50:18 +0100

    Moved Core to its own namespace
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- AmpShell.Legacy/Views/GameForm.cs
+++ AmpShell.Legacy/Views/GameForm.cs
@@ -16,8 +16,8 @@ namespace AmpShell.Legacy.Views
     using System.Text;
     using System.Windows.Forms;
 
-    using AmpShell.DAL;
-    using AmpShell.Model;
+    using AmpShell.Core.DAL;
+    using AmpShell.Core.Model;
 
     /// <summary>
     /// Form to add or modify a Game.
