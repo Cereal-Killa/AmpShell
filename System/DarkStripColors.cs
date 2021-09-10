8a992fc3ac376b371966d4999a2ab82c539c26a0
Paulo Faustino <pcfaguiar@gmail.com>
2021-09-10 07:32:06 -0300

    Discard dark mode.

---
+++ System/DarkStripColors.cs
@@ -0,0 +1,28 @@
+﻿namespace AmpShell.System
+{
+    using global::System.Drawing;
+    using global::System.Windows.Forms;
+
+    public class DarkStripColors : ProfessionalColorTable
+    {
+        public override Color MenuItemSelected
+        {
+            get { return Color.FromArgb(30, 30, 30); }
+        }
+
+        public override Color MenuItemBorder
+        {
+            get { return Color.FromArgb(30, 30, 30); }
+        }
+
+        public override Color MenuItemSelectedGradientBegin
+        {
+            get { return Color.FromArgb(30, 30, 30); }
+        }
+
+        public override Color MenuItemSelectedGradientEnd
+        {
+            get { return Color.FromArgb(30, 30, 30); }
+        }
+    }
+}
---
+++ System/DarkStripRenderer.cs
@@ -0,0 +1,12 @@
+﻿namespace AmpShell.System
+{
+    using global::System.Windows.Forms;
+
+    public class DarkStripRenderer : ToolStripProfessionalRenderer
+    {
+        public DarkStripRenderer()
+            : base(new DarkStripColors())
+        {
+        }
+    }
+}
